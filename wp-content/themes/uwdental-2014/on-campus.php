810640293a6faf9cdd9d5f312158a2eb852777d8
anyalynn <anyal@uw.edu>
2015-08-10 12:22:57 -0700

    New Hometile

--- wp-content/themes/uwdental-2014/on-campus.php
+++ wp-content/themes/uwdental-2014/on-campus.php
@@ -5,7 +5,7 @@
 						<div class="box-outer">
 							<div class="box three">
                             <div class="tile">
-                                 <?php $hometilestwo =  get_post(14059);
+                                 <?php $hometilestwo =  get_post(14267);
 								 $metatwo = get_post_meta($hometilestwo->ID);
 								 $thecontenttwo = $hometilestwo->post_content; 
 								 $posttitletwo = $hometilestwo->post_title;
