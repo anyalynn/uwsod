c3d25d63fb54c7b9f8461e3a98583ac879bfae0e
anyalynn <anyal@uw.edu>
2015-09-17 14:45:47 -0700

    Fixed footer links after theme was updated

--- wp-content/themes/uwsod-2014/footer.php
+++ wp-content/themes/uwsod-2014/footer.php
@@ -22,11 +22,10 @@
 
         <nav role="navigation" aria-label="footer links">
             <ul class="footer-links">
-                <li><a href="http://www.uw.edu/accessibility">Accessibility</a></li>
-                <li><a href="http://uw.edu/home/siteinfo/form">Contact Us</a></li>
-                <li><a href="http://www.washington.edu/jobs">Jobs</a></li>
-                <li><a href="http://www.washington.edu/safety">Campus Safety</a></li>
-                <li><a href="http://myuw.washington.edu/">My UW</a></li>
+               <li><a href="http://www.uw.edu/accessibility">Accessibility</a></li>
+                <li><a href="//dental.washington.edu/about-us/contact-us/">Contact Us</a></li>
+                  <li><a href="http://www.washington.edu/safety">Campus Safety</a></li>
+                <li><a href="https://uwnetid.sharepoint.com/sites/sod/">MyDental</a></li>
                 <li><a href="http://www.washington.edu/admin/rules/wac/rulesindex.html">Rules Docket</a></li>
                 <li><a href="http://www.washington.edu/online/privacy/">Privacy</a></li>
                 <li><a href="http://www.washington.edu/online/terms/">Terms</a></li>
