1bded3169380c1e66cd863261a22303a7755045a
anyalynn <anyal@uw.edu>
2015-10-06 13:30:10 -0700

    fixing recaptcha

--- wp-content/converge/converge.php
+++ wp-content/converge/converge.php
@@ -13,6 +13,23 @@ $ssl_transaction_type = 'CCSALE';
 $ssl_card_number = '';
 $ssl_cvv2cvc2_indicator = '1';
 
+$captcha;
+ if(isset($_POST['g-recaptcha-response'])){
+          $captcha=$_POST['g-recaptcha-response'];
+ }
+ if(!$captcha){
+   echo '<h2>Please check the the captcha form.</h2>';
+   exit;
+        }
+$response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfbQg4TAAAAAFE-DzhekhONvtYR62gGmvb6rHJu&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+if($response.success==false)
+ {
+    echo '<h2>You are spammer ! Get the @$%K out</h2>';
+  }else
+ {
+  echo '<h2>Thanks for posting comment.</h2>';
+}
+
 //Declares base URL in the event that you are using the VMpayment form.
 if($ssl_show_form == 'true')
 {
