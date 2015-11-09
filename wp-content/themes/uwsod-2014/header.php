f3974871b305d07efe5cad7fea9661152e3e9d27
anyalynn <anyal@uw.edu>
2015-11-09 07:44:59 -0800

    moved alumni renewal javascript

--- wp-content/themes/uwsod-2014/header.php
+++ wp-content/themes/uwsod-2014/header.php
@@ -16,63 +16,6 @@
             <script src="<?php bloginfo("template_directory"); ?>/assets/ie/js/respond.js" type="text/javascript"></script>
             <link rel='stylesheet' href='<?php bloginfo("template_directory"); ?>/assets/ie/css/ie.css' type='text/css' media='all' />
         <![endif]-->
-<script type="text/javascript">
-function Mem1(event){
-$("#ItemCost1").val("95.00");
-$("#userMemType").val("Regular Member");
-}
-function Mem2(event){
-$("#ItemCost1").val("50.00");
-$("#userMemType").val("New Member");
-}
-function Mem3(event){
-$("#ItemCost1").val("95.00");
-$("#userMemType").val("Associate Member");
-}
-
-function Ann(event){
-$("#ItemQty2").val(event);
-}
-
-function formVal()
-{
-	$("#ItemDesc1").val($("#userMemType").val() + "-" + $("#userAlumMemName").val() + "-" + $("#userGradYr").val());
-	$("#BillEmail").val($("#userEmail").val());
-	var bool=true;
-	if(document.getElementById("userMemType").value == "")
-		{
-			inval.style.display = 'block';
-			mtypetext.style.fontWeight = 'bold';
-			bool=false;
-		}
-	if(document.getElementById("userAlumMemName").value == "")
-		{
-			inval.style.display = 'block';
-			almemtxt.style.fontWeight = 'bold';
-			bool=false;
-		}
-	if(document.getElementById("userEmail").value == "")
-		{
-			inval.style.display = 'block';
-			emtxt.style.fontWeight = 'bold';
-			bool=false;
-		}
-	if(document.getElementById("userGradYr").value == "")
-		{
-			inval.style.display = 'block';
-			gradyrtxt.style.fontWeight = 'bold';
-			bool=false;
-		}
-	if(document.getElementById("ItemQty2").value == "")
-		{
-			inval.style.display = 'block';
-			annchgtxt.style.fontWeight = 'bold';
-			bool=false;
-		}
-	return bool;
-}
-
-</script>
     </head>
     <!--[if lt IE 9]> <body <?php body_class('lt-ie9'); ?>> <![endif]-->
     <!--[if gt IE 8]><!-->
