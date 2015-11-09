ed3c7fb9a60361f75a32e6cf1232544de013d749
anyalynn <anyal@uw.edu>
2015-11-09 13:44:36 -0800

    Updating alumni renewal amount

--- wp-content/themes/uwsod-2014/js/site.dev.js
+++ wp-content/themes/uwsod-2014/js/site.dev.js
@@ -13067,7 +13067,7 @@ UW.Social = Backbone.View.extend({
 })
 ;}).call(this)
 ;function Mem1(event){
-$("#ItemCost1").val("95.00");
+$("#ItemCost1").val("120.00");
 $("#userMemType").val("Regular Member");
 }
 function Mem2(event){
@@ -13075,7 +13075,7 @@ $("#ItemCost1").val("50.00");
 $("#userMemType").val("New Member");
 }
 function Mem3(event){
-$("#ItemCost1").val("95.00");
+$("#ItemCost1").val("120.00");
 $("#userMemType").val("Associate Member");
 }
 
