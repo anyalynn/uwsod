29905207acb2f212cf58cab2e08fc77f1dc6b1de
anyalynn <anyal@uw.edu>
2015-12-14 14:15:42 -0800

    New wire sculpture hometile.

--- wp-content/themes/uwsod-2014/on-campus.php
+++ wp-content/themes/uwsod-2014/on-campus.php
@@ -5,7 +5,7 @@
 						<div class="box-outer">
 							<div class="box three">
                             <div class="tile">
-                                 <?php $hometilestwo =  get_post(15558);
+                                 <?php $hometilestwo =  get_post(15803);
 								 $metatwo = get_post_meta($hometilestwo->ID);
 								 $thecontenttwo = $hometilestwo->post_content; 
 								 $posttitletwo = $hometilestwo->post_title;
@@ -28,7 +28,7 @@
 
 									</div>
 								<div class="tile">
-                                 <?php $hometilesone =  get_post(15543);
+                                 <?php $hometilesone =  get_post(15558);
 								 $meta = get_post_meta($hometilesone->ID);
 								 $thecontentone = $hometilesone->post_content; 
 								 $posttitleone = $hometilesone->post_title;
