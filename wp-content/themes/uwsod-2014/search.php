266f08f755b3e112cc3608ed2e85b06136383d2f
anyalynn <anyal@uw.edu>
2016-02-25 12:36:48 -0800

    search

--- wp-content/themes/uwsod-2014/search.php
+++ wp-content/themes/uwsod-2014/search.php
@@ -1,16 +1,8 @@
-<html>
+<!DOCTYPE html>
 <body>
 
-<div class="container uw-body">
 
-	<div class="row">
-
-    	<div class="col-md-12 uw-content" role='main'>
-
-     		 <?php uw_site_title(); ?>
-		     <?php if (is_front_page()) { get_template_part( 'menu', 'mobile' ); }?>
-    	     <div id='main_content' class="uw-body-copy" tabindex="-1">
-    		   	<script>
+	   	<script>
   (function() {
     var cx = '013161175624235233741:pwunvv-3nuo';
     var gcse = document.createElement('script');
@@ -23,11 +15,7 @@
   })();
 </script>
 <gcse:search></gcse:search>
-	 		</div>
- 		</div>
-	</div>
-</div>
-
+	 
 </body>
 </html>
 
