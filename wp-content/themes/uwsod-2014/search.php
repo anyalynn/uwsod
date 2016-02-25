c5c688fe2db212e081c5502bd7ada8e262df7565
anyalynn <anyal@uw.edu>
2016-02-25 13:53:17 -0800

    resetting everything

--- wp-content/themes/uwsod-2014/search.php
+++ wp-content/themes/uwsod-2014/search.php
@@ -1,63 +1,9 @@
 <!DOCTYPE html>
 <head>
-<style>
-input[type="search"].gsc-input {
-  -webkit-appearance: textfield;
-  -moz-box-sizing: content-box;
-  -webkit-box-sizing: content-box;
-  box-sizing: content-box;
-}
-input[type="search"].gsc-input::-webkit-search-cancel-button,
-input[type="search"].gsc-input::-webkit-search-decoration {
-  -webkit-appearance: none;
-}
-input[type="search"] {
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-}
-.gsc-search-box td.gsc-input {
-	font-size:15px;
-}
-input.gsc-input {
-	line-height: 45px;
-	font-size:15px;
-	display: block;
-  height: 54px;
-  outline: 2px solid #808080;
-  background: url(https://dental.washington.edu/wp-content/themes/uwsod-2014/assets/svg/search.svg) no-repeat center center #808080;
-  position: relative;
-  -webkit-background-size: 22px;
-  -moz-background-size: 22px;
-  -o-background-size: 22px;
-  background-size: 22px;
-  padding: 0px 60px 10px 0px;
-}
-body {
-	font-size:15px;
-}
-input.gsc-search-button {
-	padding-right: 78px;
-  border: 2px solid white;
-  background-color: #777;
-  -webkit-border-radius: 100%;
-  -moz-border-radius: 100%;
-  border-radius: 100%;
-  -moz-background-clip: padding;
-  -webkit-background-clip: padding-box;
-  background-clip: padding-box;
-  width: 45px;
-  padding: 0;
-  height: 45px;
-  margin: 0 15px;
-  overflow: hidden;
-}
-</style>
-</head>
 <body>
 
 
-	   	<script>
+<script>
   (function() {
     var cx = '013161175624235233741:pwunvv-3nuo';
     var gcse = document.createElement('script');
@@ -69,10 +15,9 @@ input.gsc-search-button {
     s.parentNode.insertBefore(gcse, s);
   })();
 </script>
-<div class="gcse-searchbox" data-resultsUrl="https://dental.washington.edu/wp-content/themes/uwsod-2014/search.php"
-data-newWindow="false" data-queryParameterName="search" >
+
 <gcse:search></gcse:search>
-</div>	 
+ 
 </body>
 </html>
 
