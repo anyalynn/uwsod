e9701803a32013069f548e35e72da0537b24d5ba
anyalynn <anyal@uw.edu>
2016-02-26 13:40:46 -0800

    Updating search styles

--- wp-content/themes/uwsod-2014/search.php
+++ wp-content/themes/uwsod-2014/search.php
@@ -1,5 +1,83 @@
 <!DOCTYPE html>
 <head>
+<style>
+
+#___gcse_0 .gsc-control-wrapper-cse  {
+    width: 50%; /* make sure you don't use inline width */
+   
+	
+}
+#___gcse_0 .gsc-search-button input.gsc-search-button, #___gcse_0 input.gsc-search-button {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: content-box;
+    box-sizing: content-box;
+	height:35px;
+	padding:5px;
+	font-size:15px;
+	-webkit-border-top-right-radius: 3px;
+-webkit-border-bottom-right-radius: 3px;
+-moz-border-radius-topright: 3px;
+-moz-border-radius-bottomright: 3px;
+border-top-right-radius: 3px;
+border-bottom-right-radius: 3px;
+-webkit-border-top-left-radius: 0px;
+-webkit-border-bottom-left-radius: 0px;
+-moz-border-radius-topleft: 0px;
+-moz-border-radius-bottomleft: 0px;
+border-top-left-radius: 0px;
+border-bottom-left-radius: 0px;
+  
+   border-left: none;
+	color:#fff;
+	background-color:#4b2e83;
+	border:#ccc 1px solid;
+}
+
+#___gcse_0 > .gsc-input {
+	height:35px;
+	padding:0;
+	
+	margin-right:5px;
+	
+}
+
+
+#___gcse_0 form.gsc-search-box input.gsc-input {
+	padding:5px 10px;
+	 background: none repeat scroll 0% 0% white !important;
+	height:35px;
+	font-size:15px; !important;
+}
+
+  
+#___gcse_0 .gs-title a { 
+        font-size: 16px;  
+}
+    
+  
+#___gcse_0 .gs-snippet {
+        font-size: 14px;
+		line-height:1.2em;
+}
+    
+   
+#___gcse_0 .gsc-url-top, #___gcse_0 .gsc-url-bottom {
+        display: none;
+}
+  
+
+ 
+#___gcse_0 .gs-webResult {
+        border-bottom: 1px solid #eee;
+        padding-bottom: 1em;
+    }
+    
+#___gcse_0 .gsc-result-info, #___gcse_0 .gcsc-branding {
+        display: none; 
+   
+}
+</style>
+</head>
 <body>
 
 
