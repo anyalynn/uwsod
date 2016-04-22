31cc3a422c50b814f330bb0704df19dea32148dd
anyalynn <anyal@uw.edu>
2016-04-22 11:35:17 -0700

    updated accordion, git

--- wp-content/themes/uwsod-2014/js/site.dev.js
+++ wp-content/themes/uwsod-2014/js/site.dev.js
@@ -13289,21 +13289,7 @@ UW.Social = Backbone.View.extend({
   },
 
 })
-;// This file edits the Live Theme Customizer's control menu
-//
-
-
-jQuery( document ).ready( function($) {
-	
-	// HEADERS TO HTTPS
-	$( "#accordion-section-header_image img" ).each(function() { 
-		if (!this.src.includes("https:") && window.location.href.includes(".edu")) {
-			this.src = this.src.replace( "http:" , "https:" );
-		}
-	});
-
-	
-} );;}).call(this)
+;}).call(this)
 ;function Mem1(event){
 $("#ItemCost1").val("120.00");
 $("#userMemType").val("Regular Member");
