ba95c2ab4a660b74d2e86e79bc68c83bf2d6bc5c
anyalynn <anyal@uw.edu>
2016-06-01 13:06:46 -0700

    Changed to SOD only content

--- wp-content/themes/uwsod-2014/footer.php
+++ wp-content/themes/uwsod-2014/footer.php
@@ -1,6 +1,6 @@
     <div role="contentinfo" class="uw-footer">
 
-        <a href="//dental.washington.edu" class="footer-wordmark">University of Washington</a>
+        <a href="//dental.washington.edu" class="footer-wordmark">UW School of Dentistry</a>
 
         <a href="http://www.washington.edu/boundless/"><h3 class="be-boundless">Be boundless</h3></a>
 
@@ -8,14 +8,7 @@
 
         <nav role="navigation" aria-label="social networking">
             <ul class="footer-social">
-                <li><a class="facebook" href="https://www.facebook.com/pages/The-University-of-Washington-School-of-Dentistry/160752227303910">Facebook</a></li>
-                <li><a class="twitter" href="http://twitter.com/UW">Twitter</a></li>
-                <li><a class="instagram" href="http://instagram.com/uofwa">Instagram</a></li>
-                <li><a class="tumblr" href="http://uofwa.tumblr.com/">Tumblr</a></li>
-                <li><a class="youtube" href="http://www.youtube.com/user/uwhuskies">YouTube</a></li>
-                <li><a class="linkedin" href="http://www.linkedin.com/company/university-of-washington">LinkedIn</a></li>
-                <li><a class="pinterest" href="http://www.pinterest.com/uofwa/">Pinterest</a></li>
-                <li><a class="vine" href="https://vine.co/uofwa">Vine</a></li>
+                <li><a class="facebook" href="https://www.facebook.com/pages/The-University-of-Washington-School-of-Dentistry/160752227303910">Facebook</a></li>          
                 <li><a class="google" href="https://plus.google.com/b/108816227097181482651/108816227097181482651/about?cfem=1">Google+</a></li>
             </ul>
         </nav>
@@ -32,7 +25,7 @@
             </ul>
         </nav>
 
-        <p>&copy; <?php echo date("Y"); ?> University of Washington  |  Seattle, WA</p>
+        <p>&copy; <?php echo date("Y"); ?> UW School of Dentistry  |  Seattle, WA</p>
 
     </div>
 
