d568c39f0de93683aed88c44fd52fb90bac024b6
anyalynn <anyal@uw.edu>
2016-07-25 14:08:22 -0700

    Updated hometile

--- wp-content/themes/uwsod-2014/on-campus.php
+++ wp-content/themes/uwsod-2014/on-campus.php
@@ -5,7 +5,7 @@
 						<div class="box-outer">
 							<div class="box three">
                             <div class="tile">
-                                 <?php $hometilestwo =  get_post(20349);
+                                 <?php $hometilestwo =  get_post(20417);
 								 $metatwo = get_post_meta($hometilestwo->ID);
 								 $thecontenttwo = $hometilestwo->post_content; 
 								 $posttitletwo = $hometilestwo->post_title;
@@ -28,7 +28,7 @@
 
 									</div>
 								<div class="tile">
-                                 <?php $hometilesone =  get_post(19643);
+                                 <?php $hometilesone =  get_post(20349);
 								 $meta = get_post_meta($hometilesone->ID);
 								 $thecontentone = $hometilesone->post_content; 
 								 $posttitleone = $hometilesone->post_title;
