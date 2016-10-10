66f146d8c10c550d195ffd978de682a59f493f92
anyalynn <anyal@uw.edu>
2016-10-10 13:13:03 -0700

    more

--- wp-content/themes/uwsod-2014/setup/class.dropdowns_walker.php
+++ wp-content/themes/uwsod-2014/setup/class.dropdowns_walker.php
@@ -63,7 +63,7 @@ class UW_Dropdowns_Walker_Menu extends Walker_Nav_Menu
 		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
 		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
 
-		$attributes .= $depth == 0 && $item->has_children ? ' class="has-submenu"' : '';
+		$attributes .= $item->has_children ? ' class="has-submenu"' : '';
 
 		$attributes .= $depth == 1                ? ' tabindex="-1" '                                : '';
 		$attributes .= ' title="'. $title .'" ';
