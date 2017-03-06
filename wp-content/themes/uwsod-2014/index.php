fa03d63a1e13186437fedd737d86625d0293ba30
anyalynn <anyal@uw.edu>
2017-03-06 14:24:57 -0800

    removed site title error?

--- wp-content/themes/uwsod-2014/index.php
+++ wp-content/themes/uwsod-2014/index.php
@@ -8,9 +8,7 @@
 
     <div <?php if(function_exists('uw_content_class')){uw_content_class();} ?> role='main'>
 
-      <?php uw_site_title(); ?>
-
-      <?php get_template_part('menu', 'mobile'); ?>
+     <?php get_template_part('menu', 'mobile'); ?>
 
       <?php get_template_part( 'breadcrumbs' ); ?>
 
