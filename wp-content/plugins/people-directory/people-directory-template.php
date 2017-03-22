09f230ae012a49f7a84addd0a9041ade41b7afb4
anyalynn <anyal@uw.edu>
2017-03-22 09:52:40 -0700

    Fixed directory template

--- wp-content/plugins/people-directory/people-directory-template.php
+++ wp-content/plugins/people-directory/people-directory-template.php
@@ -9,153 +9,128 @@ include 'template_functions.php';
 
 <?php get_header(); ?>
 
-		<?php get_template_part( 'header', 'image' ); ?>
+<?php get_template_part( 'header', 'image' ); ?>
 
 
 <div class="container uw-body" >
 
-  <div class="row">
+	<div class="row">
+		
+		<div class="col-md-12 uw-content" role='main' id="main_content">
+	
+			<div class="uw-body-copy">
 
-    <div class="col-md-12 uw-content" role='main' id="main_content">
-
-      <a href=" <?php echo home_url('/'); ?> " title="<?php echo esc_attr( get_bloginfo() ) ?>"><h2 class="uw-site-title"><?php bloginfo(); ?></h2></a>
-
-     
-
-      <div class="uw-body-copy">
-
-
-        <?php
-          // Start the Loop.
-          while ( have_posts() ) : the_post();
-          ?>
-          
-				
-		<?php uw_breadcrumbs(); ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="entry-header">
-				<h1 class="hide"><?php echo apply_filters('italics', get_the_title()); ?></h1>
-			</header><!-- .entry-header -->        
-			<?php
-				$args = array('post_type' => 'people', 'posts_per_page' => -1);
-				$query = new WP_Query($args);
-				$people = $query->get_posts();
-				usort($people, 'last_name_sort');
-				$people[0]->post_title;
-				  $name_link = true;
-			?>  
-		<div>	
-			<div id="livesearchdiv">
-				<input id='livesearch' class="form-control" type="search" placeholder="Name/Department/Role..." name="filter" />
-			</div>	
-		</div> <!-- filter -->
-    <?php
-    
-	   	    $teams = dont_group_by_team($people); 
-	   
-            foreach($teams as $team => $people):
-               if (count($people) != 0): 	//just in case there are zero people in a manually specified team (or Team No-Team) 
-{ ?>
-                   <div class='searchable-container'>
-                    <?php foreach ($people as $person):
-                        $personID = $person->ID;
-                        $name = $person->post_title;
-                        $main_pic = get_post_meta($personID, 'main_pic', true);
-                        $position = get_post_meta($personID, 'position', true);
-						$position2 = get_post_meta($personID, 'position2', true);
-						$research = get_post_meta($personID, 'research', true);
-                        $phone = get_post_meta($personID, 'phone', true);
-                        $email = get_post_meta($personID, 'email', true);
-                        $person_teams_arr = get_the_terms($personID, 'teams');
-						$team_class = '';
-                        $person_teams = '';
-						$content=$person->post_content;
-                        if (!empty($person_teams_arr)) {
-							$count = 0;
-                            foreach ($person_teams_arr as $person_teams_item) {
-								if ($person_teams_item->name == 'Faculty') {
-									$team_class  .= ' faculty ';
-								}
-								if ($person_teams_item->name == 'Staff') {
-									$team_class  .= ' staff ';
-								}
-								if ($person_teams_item->name == 'Affiliate Faculty') {
-									$team_class  .= ' affiliate ';
-								}
-								if ($person_teams_item->name == 'Endodontics') {
-									$team_class  .= ' endo ';
-								}
-								if ($person_teams_item->name == 'Periodontics') {
-									$team_class .= ' perio ';
-								}
-								if ($person_teams_item->name == 'Pediatric Dentistry') {
-									$team_class .= ' pedo ';
-								}
-								if ($person_teams_item->name == 'Restorative Dentistry') {
-									$team_class .= ' restore ';
-								}
-								if ($person_teams_item->name == 'Oral Health Sciences') {
-									$team_class .=  ' ohs ';
-								}
-								if ($person_teams_item->name == 'Oral and Maxillofacial Surgery') {
-									$team_class .=  ' oms ';
-								}
-								if ($person_teams_item->name == 'Oral Medicine') {
-									$team_class .= ' oralmed ';
-								}
-								if ($person_teams_item->name == 'Orthodontics') {
-									$team_class .= ' ortho ';
-								}
-								if($count == 0)
-								{	
-                               		 $person_teams = $person_teams . ' ' . $person_teams_item->name;
-								}
-								else
-									$person_teams = $person_teams . ', ' . $person_teams_item->name;
-								
-								$count++;
-                            }
-                        }
-                        ?>
-                        <div class='profile-list searchable <?= $team_class ?> '>
-                                         					  
-						  
-						  <div class="info-wrapper">
-						   <img <?php if (empty($main_pic)) { ?> class='no-pic'<?php } ?> src='<?= $main_pic ?>' alt='<?= $name ?>' class='pic'/>
-								<div class='info'>
-									<?php if (($name_link)){
-										?><a href="<?= get_permalink($personID) ?>"><?php
-									} ?>
-									<h3 class='name search-this'><?= $name ?></h3></a>
-									
-									<p class='title search-this'><?= $position  ?><br /><?= $position2 ?></p>                                            
-									<p> <?= $phone ?> </p>
-									<?php if (($email)) 
-									{ ?>
-									<p><a href="mailto:<?= $email ?>"><?= $email ?></a></p> 
-									<?php } ?>
-								</div>
-								<?php if (!empty($research)){ ?>
-									<div class="research">
-									<h3 class="name research-header">Research Interests</h3> 
-										<p class="research-interests"> <?= $research ?> </p>
-									</div>
-								<?php } ?>
-						  </div>			
-                        </div>
-                    <?php  endforeach; ?>
-                       
-                    </div>
-                
-                <?php } endif; 
-            endforeach; ?>
-            
-        </article>
-        <?php
-        endwhile;
-        ?>
-       </div>
-      </div>
-	</div> 
-  </div>
+        	<?php
+          	// Start the Loop.
+				while ( have_posts() ) : the_post();
+          	?>
+					<?php uw_breadcrumbs(); ?>
+					<h1><?php the_title() ?></h1>
+					<?php	$args = array('post_type' => 'people', 'posts_per_page' => -1);
+					$query = new WP_Query($args);
+					$people = $query->get_posts();
+					usort($people, 'last_name_sort');
+					$people[0]->post_title;
+					$name_link = true;
+				?>  
+				<div id="livesearchdiv">
+					<input id='livesearch' class="form-control" type="search" placeholder="Name/Department/Role..." name="filter" />
+				</div>	
+	
+				<?php
+    			$teams = dont_group_by_team($people); 
+				foreach($teams as $team => $people):
+    			if (count($people) != 0): 	//just in case there are zero people in a manually specified team (or Team No-Team) 
+				{ ?>
+    			<div class='searchable-container'>
+    				<?php foreach ($people as $person):
+            		$personID = $person->ID;
+	 				$name = $person->post_title;
+				 	$main_pic = get_post_meta($personID, 'main_pic', true);
+				 	$position = get_post_meta($personID, 'position', true);
+					$position2 = get_post_meta($personID, 'position2', true);
+					$research = get_post_meta($personID, 'research', true);
+				 	$phone = get_post_meta($personID, 'phone', true);
+				 	$email = get_post_meta($personID, 'email', true);
+				 	$person_teams_arr = get_the_terms($personID, 'teams');
+					$team_class = '';
+            		$person_teams = '';
+					$content=$person->post_content;
+				 	if (!empty($person_teams_arr)) {
+						$count = 0;
+						foreach ($person_teams_arr as $person_teams_item) {
+						if ($person_teams_item->name == 'Faculty') {
+							$team_class  .= ' faculty ';
+						}
+						if ($person_teams_item->name == 'Staff') {
+							$team_class  .= ' staff ';
+						}
+						if ($person_teams_item->name == 'Affiliate Faculty') {
+						$team_class  .= ' affiliate ';
+						}
+						if ($person_teams_item->name == 'Endodontics') {
+							$team_class  .= ' endo ';
+						}
+						if ($person_teams_item->name == 'Periodontics') {
+							$team_class .= ' perio ';
+						}
+						if ($person_teams_item->name == 'Pediatric Dentistry') {
+							$team_class .= ' pedo ';
+						}
+						if ($person_teams_item->name == 'Restorative Dentistry') {
+							$team_class .= ' restore ';
+						}
+						if ($person_teams_item->name == 'Oral Health Sciences') {
+							$team_class .=  ' ohs ';
+						}
+						if ($person_teams_item->name == 'Oral and 	Maxillofacial Surgery') {
+							$team_class .=  ' oms ';
+						}
+						if ($person_teams_item->name == 'Oral Medicine') {
+							$team_class .= ' oralmed ';
+						}
+						if ($person_teams_item->name == 'Orthodontics') {
+							$team_class .= ' ortho ';
+						}
+						if($count == 0)
+						{	
+							$person_teams = $person_teams . ' ' . $person_teams_item->name;
+						}
+						else
+							$person_teams = $person_teams . ', ' . $person_teams_item->name;
+					$count++;
+            		}
+				}
+				?>
+        		<div class='profile-list searchable <?= $team_class ?> '>
+        			<div class="info-wrapper">
+			 			<img <?php if (empty($main_pic)) { ?> class='no-pic'<?php } ?> src='<?= $main_pic ?>' alt='<?= $name ?>' class='pic'/>
+						<div class='info'>	
+							<?php if (($name_link)){
+							?><a href="<?= get_permalink($personID) ?>"><?php
+							} ?>
+							<h3 class='name search-this'><?= $name ?></h3></a>
+							<p class='title search-this'><?= $position  ?><br /><?= $position2 ?></p>                             <p> <?= $phone ?> </p>
+							<?php if (($email)) 
+							{ ?>
+							<p><a href="mailto:<?= $email ?>"><?= $email ?></a></p> 
+							<?php } ?>
+						</div>  
+					<?php if (!empty($research)){ ?>
+						<div class="research">
+							<h3 class="name research-header">Research Interests</h3> 
+							<p class="research-interests"> <?= $research ?> </p>
+						</div> 
+					<?php } ?>
+			  		</div>	 		
+         		 </div> 
+         		<?php  endforeach; ?>
+       		</div> 
+				<?php } endif; 
+				endforeach; ?>
+ 				<?php endwhile; ?>
+		 </div> 
+	</div>
+</div> 
+</div> 
 <?php get_footer(); ?>
