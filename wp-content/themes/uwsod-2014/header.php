fc6a3c565336ed18c092487a78f01b7b9f0add0a
anyalynn <anyal@uw.edu>
2017-06-21 07:53:01 -0700

    Put Skip link above Search

--- wp-content/themes/uwsod-2014/header.php
+++ wp-content/themes/uwsod-2014/header.php
@@ -32,10 +32,9 @@
     <!--[if gt IE 8]><!-->
     <body <?php body_class(); ?> >
     <!--<![endif]-->
+    <a id="main-content" href="#main_content" class='screen-reader-shortcut'>Skip to main content</a>
 
     <div id="uwsearcharea" aria-hidden="true" class="uw-search-bar-container"></div>
-
-    <a id="main-content" href="#main_content" class='screen-reader-shortcut'>Skip to main content</a>
 
     <div id="uw-container">
 
