12dc8f862ded9b6ec5318aaae2d9509170aba189
anyalynn <anyal@uw.edu>
2018-10-15 11:05:08 -0700

    Removed border="1" in JS code because it is deprecated in HTML5 and causes accessibility errors

--- wp-content/themes/uwsod-2018/js/uw.init.js
+++ wp-content/themes/uwsod-2018/js/uw.init.js
@@ -76,7 +76,7 @@ UW.initialize = function( $ )
   UW.alert = new UW.Alert({ after: UW.elements.alert, model: new UW.Alert.Model() });
 
   // todo: add to separate file
-  $('table').addClass('table table-striped').attr( "border", 1 )
+  $('table').addClass('table table-striped')
 
   $('pre').addClass('prettyprint')
 
