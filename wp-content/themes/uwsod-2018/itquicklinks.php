96295c0edbff5fda3191fff1ea515fe7975cc02d
anyalynn <anyal@uw.edu>
2018-11-16 11:54:01 -0800

    New features for IT page

---
+++ wp-content/themes/uwsod-2018/itquicklinks.php
@@ -0,0 +1,84 @@
+        <nav aria-label="popular links" class="container uw-links">
+			<ul class="center-block">
+				<li><a href="http://dental.washington.edu/it/training/" title="training"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="60px" height="60px" viewBox="0 0 60 60" enable-background="new 0 0 60 60" xml:space="preserve">
+<g>
+	
+		<circle fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="44.2" cy="25.8" r="4.2"/>
+	<path fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M51.4,33.6c-0.7-0.6-1.6-0.7-1.9-0.7v0h-5.3h-3h-0.6h-2.4h-1.3c-0.1,0-0.1,0-0.2,0h-0.4V33c-0.4,0.1-0.7,0.3-0.9,0.6l-3.5,4.8
+		l-4.5-4.6c-0.7-0.7-1.8-0.7-2.5,0s-0.7,1.8,0,2.5l6,6.1c0.3,0.3,0.8,0.5,1.3,0.5c0,0,0.1,0,0.1,0c0.5,0,1-0.3,1.3-0.7l2.8-3.8v14.8
+		H52V35C52,35,51.9,34.1,51.4,33.6z"/>
+	
+		<polygon fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+		31.8,27.3 6.9,34.4 6.9,6.8 31.8,11.6 	"/>
+	
+		<polyline fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+		15.7,18.2 15.7,23 11.7,29.2 	"/>
+	
+		<line fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="16" y1="23.2" x2="19.9" y2="26.8"/>
+	
+		<polyline fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+		10.7,15.3 15.7,18.4 20.7,15.3 	"/>
+	
+		<circle fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="15.5" cy="13.3" r="1.7"/>
+	
+		<line fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="20.7" y1="20.6" x2="24.9" y2="33.9"/>
+</g>
+</svg>
+				<span>Training</span></a> </li>
+              
+				<li><a href="http://dental.washington.edu/it/computing/" title="Computing"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+<g>
+	<g>
+		<path fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" d="M84.4,76.3H15.6c-0.2,0-0.4-0.2-0.4-0.4v-2.6
+			h69.2c0.2,0,0.4,0.2,0.4,0.4v2.3C84.8,76.1,84.6,76.3,84.4,76.3z"/>
+	</g>
+	<g>
+		<path fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" d="M75.2,58.4H25.8c-1.2,0-2.2-1-2.2-2.2V26
+			c0-1.2,1-2.2,2.2-2.2h49.5c1.2,0,2.2,1,2.2,2.2v30.2C77.5,57.4,76.5,58.4,75.2,58.4z"/>
+		<rect x="27.7" y="27.8" fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" width="45.5" height="25.8"/>
+	</g>
+	
+		<polygon fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+		84.8,72.8 15.2,72.8 23.6,58.3 77.5,58.3 	"/>
+	
+		<line fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="26.3" y1="62.6" x2="74.7" y2="62.6"/>
+	
+		<polygon fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+		56,69.5 42.8,69.5 44.6,66.5 54.7,66.5 	"/>
+</g>
+</svg><span>Computing</span></a> </li>
+                <li><a href="http://dental.washington.edu/it/email/" title="Email">
+				
+				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="60px" height="60px" viewBox="0 0 60 60" enable-background="new 0 0 60 60" xml:space="preserve">
+<g>
+	
+		<rect x="7.001" y="14.132" fill="none" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" width="45.998" height="32.078"/>
+	<polyline fill="none" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" points="7,14.132 30,37.132 53,14.132 	"/>
+	<line fill="none" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" x1="23.107" y1="30.102" x2="7" y2="46.209"/>
+	<line fill="none" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" x1="53" y1="46.21" x2="36.869" y2="30.08"/>
+</g>
+</svg>
+				<span>Email</span></a> </li>
+                 <li><a href="http://dental.washington.edu/it/security/" title="Security">
+
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+<g>
+	<path fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M54.5,73.9h-8.9c-6.5,0-11.8-5.3-11.8-11.8V32.4c0-6.5,5.3-11.8,11.8-11.8h8.9c6.5,0,11.8,5.3,11.8,11.8v29.7
+		C66.2,68.6,60.9,73.9,54.5,73.9z"/>
+	<path fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M55,72.4H45c-4.4,0-8-3.6-8-8v-32c0-4.4,3.6-8,8-8h10c4.4,0,8,3.6,8,8v32C63,68.8,59.4,72.4,55,72.4z"/>
+	
+		<rect x="26.7" y="47" fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" width="46.7" height="32.3"/>
+	
+		<path fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="
+		M53.9,60.8c0-2.4-1.7-4.3-3.9-4.3s-3.9,1.9-3.9,4.3c0,1.9,1.1,3.4,2.5,4v5.2h2.7v-5.2C52.8,64.3,53.9,62.7,53.9,60.8z"/>
+</g>
+</svg><span>Security</span></a> 
+
+			</ul></nav>
