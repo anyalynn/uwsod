d492f54970555e5cae57e6f7a5f0a55a88ec772e
anyalynn <anyal@uw.edu>
2018-11-16 12:41:21 -0800

    centering

--- wp-content/themes/uwsod-2018/itquicklinks.php
+++ wp-content/themes/uwsod-2018/itquicklinks.php
@@ -1,6 +1,6 @@
         <nav aria-label="popular links" class="container uw-links">
 			<ul class="center-block">
-				<li><a href="http://dental.washington.edu/it/training/" title="training"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+				<li style="margin-left:200px"><a href="http://dental.washington.edu/it/training/" title="training"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 width="60px" height="50.8px" viewBox="0 0 60 50.8" enable-background="new 0 0 60 50.8" xml:space="preserve">
 <g>
 	
