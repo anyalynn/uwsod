8d77937cab3b12a7afe002dd3e656d01013db91c
anyalynn <anyal@uw.edu>
2018-11-16 12:06:56 -0800

    fixed size of computer

--- wp-content/themes/uwsod-2018/itquicklinks.php
+++ wp-content/themes/uwsod-2018/itquicklinks.php
@@ -29,25 +29,25 @@
 				<span>Training</span></a> </li>
               
 				<li><a href="http://dental.washington.edu/it/computing/" title="Computing"><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
+	 width="49.5px" height="37.9px" viewBox="0 0 49.5 37.9" enable-background="new 0 0 49.5 37.9" xml:space="preserve">
 <g>
 	<g>
-		<path fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" d="M84.4,76.3H15.6c-0.2,0-0.4-0.2-0.4-0.4v-2.6
-			h69.2c0.2,0,0.4,0.2,0.4,0.4v2.3C84.8,76.1,84.6,76.3,84.4,76.3z"/>
+		<path fill="#FFFFFF" stroke="#666666" stroke-width="2" stroke-miterlimit="10" d="M48.2,36.9h-47c-0.1,0-0.3-0.1-0.3-0.3v-1.8
+			h47.2c0.1,0,0.3,0.1,0.3,0.3v1.5C48.5,36.8,48.4,36.9,48.2,36.9z"/>
 	</g>
 	<g>
-		<path fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" d="M75.2,58.4H25.8c-1.2,0-2.2-1-2.2-2.2V26
-			c0-1.2,1-2.2,2.2-2.2h49.5c1.2,0,2.2,1,2.2,2.2v30.2C77.5,57.4,76.5,58.4,75.2,58.4z"/>
-		<rect x="27.7" y="27.8" fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-miterlimit="10" width="45.5" height="25.8"/>
+		<path fill="#FFFFFF" stroke="#666666" stroke-width="2" stroke-miterlimit="10" d="M42,24.6H8.2c-0.8,0-1.5-0.7-1.5-1.5V2.5
+			C6.7,1.7,7.4,1,8.2,1H42c0.8,0,1.5,0.7,1.5,1.5v20.6C43.5,24,42.8,24.6,42,24.6z"/>
+		<rect x="9.5" y="3.8" fill="#FFFFFF" stroke="#666666" stroke-width="2" stroke-miterlimit="10" width="31.1" height="17.6"/>
 	</g>
 	
-		<polygon fill="#FFFFFF" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
-		84.8,72.8 15.2,72.8 23.6,58.3 77.5,58.3 	"/>
+		<polygon fill="#FFFFFF" stroke="#666666" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+		48.5,34.5 1,34.5 6.7,24.6 43.5,24.6 	"/>
 	
-		<line fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="26.3" y1="62.6" x2="74.7" y2="62.6"/>
+		<line fill="none" stroke="#666666" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="8.6" y1="27.5" x2="41.6" y2="27.5"/>
 	
-		<polygon fill="none" stroke="#a3a2a3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
-		56,69.5 42.8,69.5 44.6,66.5 54.7,66.5 	"/>
+		<polygon fill="none" stroke="#666666" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+		28.9,32.2 19.8,32.2 21.1,30.2 27.9,30.2 	"/>
 </g>
 </svg><span>Computing</span></a> </li>
                 <li><a href="http://dental.washington.edu/it/email/" title="Email">
