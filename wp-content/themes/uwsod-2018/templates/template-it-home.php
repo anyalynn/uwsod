baf5650732b546fe51ed498ed35ecedf74f3f3b8
anyalynn <anyal@uw.edu>
2018-11-19 10:28:42 -0800

    removing site title

--- wp-content/themes/uwsod-2018/templates/template-it-home.php
+++ wp-content/themes/uwsod-2018/templates/template-it-home.php
@@ -25,12 +25,7 @@
 	<div class="row">
 		<div class="uw-hero-image hero-height2 <?php echo $hasmobileimage ?>" style="background-image: url(<?php echo $url ?>);">
          
-	      	<div class="container">
-           
-			  	<h1 class="uw-site-title2">Information Technology</h1>
-	        	<span class="udub-slant"><span></span></span>
-        
-			</div>
+	      	
 	 	</div>
   		<div class="row">
             
