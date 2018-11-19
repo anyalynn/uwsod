f4c7e32bb02367a210491fabae62176b1a1d96e8
anyalynn <anyal@uw.edu>
2018-11-19 11:08:30 -0800

    configuring like it-home

--- wp-content/themes/uwsod-2018/templates/template-it.php
+++ wp-content/themes/uwsod-2018/templates/template-it.php
@@ -21,66 +21,46 @@
       $buttonlink = get_post_meta($post->ID, "buttonlink");   ?>
 
 
-<div class="uw-hero-image hero-height2 <?php echo $hasmobileimage ?>" style="background-image: url(<?php echo $url ?>);">
-     <?php if( !empty($mobileimage) ) { ?>
-    <div class="mobile-image" style="background-image: url(<?php echo $mobileimage ?>);"></div>
-    <?php } ?>
-     
-      <div class="container">
-        <div id="hashtag" class="mask"><span><span><?php echo $banner[0] ? $banner[0] : ''; ?></span></span></div>
-        <?php if(!empty($banner) && $banner[0]){ ?>
-        <div id="hashtag"><span><span><?php echo $banner[0] ? $banner[0] : ''; ?></span></span></div>
-        <?php } ?>
+<div class="uw-body">
+	<div class="row">
+		<div class="uw-hero-image hero-height2 <?php echo $hasmobileimage ?>" style="background-image: url(<?php echo $url ?>);">
+         
+	      	<div class="container">
+           
+			  	<h1 >Information Technology</h1>
+	        	<span class="udub-slant"><span></span></span>
         
-        <h1 class="uw-site-title2">Information Technology</h1>
-        <span class="udub-slant"><span></span></span>
-        <?php if(!empty($buttontext) && $buttontext[0]){ ?>
-        <a class="uw-btn btn-sm btn-none" href="<?php echo $buttonlink[0] ? $buttonlink[0] : ''; ?>"><?php echo $buttontext[0] ? $buttontext[0] : ''; ?></a> 
-        <?php } ?>     
-      </div>
-</div>
+			</div>
+	 	</div>
+	    <div class="col-md-<?php echo (($sidebar[0]!="on") ? "8" : "12" ) ?> uw-content" role='main'>
+   	
+		<?php get_template_part( 'breadcrumbs' ); ?>
 
-<div class="container uw-body">
-  <div class="row">
-    <div class="hero-content col-md-<?php echo (($sidebar[0]!="on") ? "8" : "12" ) ?> uw-content" role='main'>
-   
+			<div id='main_content' class="uw-body-copy" tabindex="-1">
 
-      <?php get_template_part( 'breadcrumbs' ); ?>
+			<?php
+    	      while ( have_posts() ) : the_post(); 
 
-      <div id='main_content' class="uw-body-copy" tabindex="-1">
+        	    //the_content();
+            	get_template_part( 'content', 'page-header' );
 
+	            // If comments are open or we have at least one comment, load up the comment template.
+    	        if ( comments_open() || get_comments_number() ) {
+        	      comments_template();
+            	}
 
-    
-
-
-
-        <?php
-          while ( have_posts() ) : the_post(); 
-
-            //the_content();
-            get_template_part( 'content', 'page-header' );
-
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) {
-              comments_template();
-            }
-
-          endwhile;
+		        endwhile;
           
-        ?>
+        	?>
 
-      </div>
-
-    </div>
-
-    <div id="sidebar"><?php 
-      if($sidebar[0]!="on"){
-        get_sidebar();
-      }
-    ?></div>
-
+	      </div>
+	    </div>
+	    <div id="sidebar"><?php 
+    	  if($sidebar[0]!="on"){
+        	get_sidebar();
+	      }
+    	?></div>
   </div>
-
 </div>
 
 <?php get_footer(); ?>
