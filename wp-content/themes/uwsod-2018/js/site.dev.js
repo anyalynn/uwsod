6181cacf53bd03a742adfbb225cc4a07c4c6c3e1
anyalynn <anyal@uw.edu>
2020-03-25 15:14:59 -0700

    updates to YouTube.js

--- wp-content/themes/uwsod-2018/js/site.dev.js
+++ wp-content/themes/uwsod-2018/js/site.dev.js
@@ -11625,7 +11625,7 @@ UW.Slideshow = Backbone.View.extend({
 // This provides the structure and functionality of the UW Youtube player
 // For usage please refer to the [UW Web Youtube Player](http://uw.edu/brand/web/#youtube)
 // It can support a single youtube video or playlist embed
-// options include max results for playlists, modest youtube branding and default resolution 
+// options include max results for playlists, modest youtube branding and default resolution
 // requires a unique id for each div.uw-youtube even if there is just one
 
 //       Single: <div id='some-unique-id' class="uw-youtube" data-uw-youtube='youtube_id_here' data-uw-youtube-type='single'></div>
@@ -11646,17 +11646,17 @@ UW.YouTube.Collection = Backbone.Collection.extend({
         this.youtube_id = this.$el.data('uw-youtube');
         this.setup_for_type();
         this.make_view();
-        this.fetch({success: this.view.onDataReady});
+        this.fetch({ success: this.view.onDataReady });
     },
 
     // See if the div.uw-youtube is a playlist or single video
     // setup the proper request and model type
     // setup some other relative parameters
-    setup_for_type : function (youtube_id) {
+    setup_for_type: function (youtube_id) {
         this.type = this.$el.data('uw-youtube-type');
         this.modest = this.$el.data('modest');
         this.resolution = this.$el.data('resolution');
-        if (this.type == 'playlist'){
+        if (this.type == 'playlist') {
             this.max_results = 20;
             var max_results_temp = parseInt(this.$el.data('max-results'), 10);
             if (max_results_temp > 0) {
@@ -11675,28 +11675,28 @@ UW.YouTube.Collection = Backbone.Collection.extend({
     parse: function (response) {
         var type = this.type, youtube_id = this.youtube_id;
         return _(response.items).map(function (item) {
-            if (type == 'single'){
-                item.snippet.resourceId = {videoId: youtube_id};
+            if (type == 'single') {
+                item.snippet.resourceId = { videoId: youtube_id };
             }
             return item.snippet;
         });
     },
-    
+
     // make the view at the proper time
     make_view: function (type) {
-        this.view = new UW.YouTube.CollectionView({collection: this});
+        this.view = new UW.YouTube.CollectionView({ collection: this });
     },
 
 });
 
 // The CollectionView builds the html for the player and the control structure for the vidoes
 UW.YouTube.CollectionView = Backbone.View.extend({
-    
+
     // template that all videos get
-    template : "<div class='nc-video-player' role='region' aria-label='video' tabindex=-1><div class='tube-wrapper'></div></div>",
+    template: "<div class='nc-video-player' role='region' aria-label='video' tabindex=-1><div class='tube-wrapper'></div></div>",
 
     // playist section html that only playlists get
-    playlist_section : "<div class='vidSmall'><div class='scrollbar'><div class='track'><div class='thumb'><div class='end'></div></div></div></div><div class='viewport'><div class='vidContent overview'><ul></ul></div></div></div>",
+    playlist_section: "<div class='vidSmall'><div class='scrollbar'><div class='track'><div class='thumb'><div class='end'></div></div></div></div><div class='viewport'><div class='vidContent overview'><ul></ul></div></div></div>",
 
     //event handlers for the templated html
     events: {
@@ -11712,7 +11712,7 @@ UW.YouTube.CollectionView = Backbone.View.extend({
         this.data_ready = false;
         this.wrap();
         this.add_iFrame_api();
-        if (this.collection.type == 'playlist'){
+        if (this.collection.type == 'playlist') {
             this.$el.addClass('playlist');
             this.add_playlist_section();
             this.scrollbar_visible = false;
@@ -11723,9 +11723,9 @@ UW.YouTube.CollectionView = Backbone.View.extend({
     // a resize handler for playlists. Handles the edge case of when a container
     // is resized to be too small for the list and therefor requires a scrollbar
     // when none is present or vice versa
-    resized: function() {
+    resized: function () {
         var viewport_new_width = this.$vidSmall.find('.viewport').width();
-        if (viewport_new_width != this.$viewport_width){
+        if (viewport_new_width != this.$viewport_width) {
             this.$viewport_width = viewport_new_width;
             this.showTinyScrollbar();
         }
@@ -11740,7 +11740,7 @@ UW.YouTube.CollectionView = Backbone.View.extend({
 
     // if we don't have a copy of the youtube iframe api yet. add it
     add_iFrame_api: function () {
-        if (UW.$body.find('script#iFrame').length === 0){
+        if (UW.$body.find('script#iFrame').length === 0) {
             UW.$body.append('<script id="iFrame" title="YouTube video player" src="//www.youtube.com/player_api" type="text/javascript"></script>');
             this.add_iFrame_function();
         }
@@ -11749,15 +11749,19 @@ UW.YouTube.CollectionView = Backbone.View.extend({
     // at this point, all the collections should be created.
     // Each gets a uwplayer variable that is a YT.Player corresponding to the collection
     add_iFrame_function: function () {
-        window.onYouTubeIframeAPIReady = function() {
-            for (var i = 0, length = UW.youtube.length; i < length; i++){
+        window.onYouTubeIframeAPIReady = function () {
+            for (var i = 0, length = UW.youtube.length; i < length; i++) {
                 var collection = UW.youtube[i], player_vars = {};
                 // if the collection desires no youtube branding, set these parameters
                 if (collection.modest) {
                     player_vars = {
-                        'rel'           : 0,
-                        'controls'      : 0,
+                        'rel': 0,
+                        'controls': 0,
                         'modestbranding': 1,
+                    }
+                } else {
+                    player_vars = {
+                        'rel': 0,
                     }
                 }
                 // if (collection.resolution !== 'undefined'){
@@ -11779,7 +11783,7 @@ UW.YouTube.CollectionView = Backbone.View.extend({
 
     // This function is called if the collection is a playlist
     // adds the playlist section
-    add_playlist_section : function () {
+    add_playlist_section: function () {
         this.$el.append(this.playlist_section);
         this.$vidSmall = this.$el.find('.vidSmall');
         this.$vidContent = this.$el.find('.vidContent');
@@ -11798,7 +11802,7 @@ UW.YouTube.CollectionView = Backbone.View.extend({
     // checks to see if the iframe api is ready
     onDataReady: function () {
         this.data_ready = true;
-        if (this.collection.type == 'playlist'){
+        if (this.collection.type == 'playlist') {
             this.$vidContent_width = this.collection.models.length * 135
             this.$vidContent.width(this.$vidContent_width + 'px');
             this.$viewport_width = this.$vidSmall.find('.viewport').width();
@@ -11809,14 +11813,14 @@ UW.YouTube.CollectionView = Backbone.View.extend({
 
     // this function shows the tiny scrollbar.
     showTinyScrollbar: function () {
-        if (this.$vidContent_width > this.$viewport_width){
+        if (this.$vidContent_width > this.$viewport_width) {
             if (!this.scrollbar_visible) {
                 this.$vidSmall.find('.scrollbar').show();
-                this.$vidSmall.tinyscrollbar({axis: 'x'});
+                this.$vidSmall.tinyscrollbar({ axis: 'x' });
                 this.scrollbar_visible = true;
             }
         }
-        else if (this.scrollbar_visible){
+        else if (this.scrollbar_visible) {
             this.$vidSmall.find('.scrollbar').removeAttr('style');
             this.scrollbar_visible = false;
         }
@@ -11824,17 +11828,17 @@ UW.YouTube.CollectionView = Backbone.View.extend({
 
     // this function checks the state of data/player to prevent a race case.
     // Both the data and the player must be ready to go.  Then we play the correct video
-    check_all_ready: function() {
-        if (this.data_ready && this.player_ready){
+    check_all_ready: function () {
+        if (this.data_ready && this.player_ready) {
             this.play(this.collection.models[0].get('resourceId').videoId);
-        } 
+        }
     },
 
     // when the player changes state, this is run.
     // Currently stuff only happens if this is a playlist
     // TODO: add a publicly visible event on video end for showcase pages
     onStateChange: function (event) {
-        if (this.is_playlist) { 
+        if (this.is_playlist) {
             //event.data is 0 when a video finishes playing.  Find out what video we just finished, then play the next one or loop back to the beginning of the playlist
             if (event.data === 0) {
                 var video = this.$vidContent.find('.vid-active').attr('id');
@@ -11851,7 +11855,7 @@ UW.YouTube.CollectionView = Backbone.View.extend({
 
     // play the video id passed. If 'playnow' not passed, assume false.
     // If 'playnow' is true play the video, otherwise just cue it up
-    play: function (id, playnow){
+    play: function (id, playnow) {
         playnow = playnow || false;
         if (playnow) {
             this.uwplayer.loadVideoById(id);
@@ -11866,12 +11870,12 @@ UW.YouTube.CollectionView = Backbone.View.extend({
             var $small = $('#' + id);
             $small.addClass('vid-active');
             this.$el.attr('aria-label', 'video: ' + $small.data('title'));
-            if (this.scrollbar_visible){
+            if (this.scrollbar_visible) {
                 var leftpos = $small.position().left;
-                if (this.$vidContent_width - leftpos < this.$viewport_width){
+                if (this.$vidContent_width - leftpos < this.$viewport_width) {
                     leftpos = this.$vidContent_width - this.$viewport_width;
                 }
-                this.$vidContent.animate({left: -leftpos}, 500);
+                this.$vidContent.animate({ left: -leftpos }, 500);
                 this.$vidSmall.data('plugin_tinyscrollbar').update(leftpos);
             }
         }
@@ -11887,8 +11891,8 @@ UW.YouTube.CollectionView = Backbone.View.extend({
 // Video is a model for a single video
 UW.YouTube.Video = Backbone.Model.extend({
     initialize: function () {
-        if (this.get('resourceId')){
-            this.view = new UW.YouTube.VideoView({model: this});
+        if (this.get('resourceId')) {
+            this.view = new UW.YouTube.VideoView({ model: this });
         }
     }
 });
@@ -11896,7 +11900,7 @@ UW.YouTube.Video = Backbone.Model.extend({
 // Video View is a view for single video. Currently does nothing
 UW.YouTube.VideoView = Backbone.View.extend({
     //template: underscore + html string here,
-    
+
     initialize: function () {
         this.render();
     },
@@ -11914,8 +11918,8 @@ UW.YouTube.PlaylistItem = Backbone.Model.extend({
 
     // if the video is a real video and not an error code, make a view
     initialize: function () {
-        if (this.get('resourceId')){
-            this.view = new UW.YouTube.PlaylistItemView({model:this});
+        if (this.get('resourceId')) {
+            this.view = new UW.YouTube.PlaylistItemView({ model: this });
         }
     },
 });
@@ -11936,7 +11940,7 @@ UW.YouTube.PlaylistItemView = Backbone.View.extend({
     // gets the data ready, templates it, then appends to the playlist section
     render: function () {
         var item = this.model.toJSON();
-        var small_vid = _.template(this.template)( item );
+        var small_vid = _.template(this.template)(item);
         this.$el.append(small_vid);
     },
 });
