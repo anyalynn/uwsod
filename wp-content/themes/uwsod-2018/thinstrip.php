c44256d13f80105298c6133e91dbef807839bac6
anyalynn <anyal@uw.edu>
2021-02-04 13:04:01 -0800

    added class gift to make a gift for google tag

--- wp-content/themes/uwsod-2018/thinstrip.php
+++ wp-content/themes/uwsod-2018/thinstrip.php
@@ -7,7 +7,7 @@
   <div class='align-right'>
        <nav class="uw-thin-strip-nav" aria-label='audience menu'>
           <ul class="uw-thin-links">
-             <li class="menu-give-now"><a href="//dental.washington.edu/alumni-friends/give/make-a-gift/" title="Make a Gift">Make a Gift</a></li>
+             <li class="menu-give-now"><a href="//dental.washington.edu/alumni-friends/give/make-a-gift/" class="gift" title="Make a Gift">Make a Gift</a></li>
              <li class="menu-myuw-giving"><a href="//dental.washington.edu/patient/make-an-appointment/" title="Make an Appointment">New Patients</a></li>
           </ul>
       </nav>
