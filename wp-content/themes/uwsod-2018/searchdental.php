7093473a6ebc626cd831ba77aa57184c3261549a
anyalynn <anyal@uw.edu>
2021-02-05 07:47:05 -0800

    Fixed search header

--- wp-content/themes/uwsod-2018/searchdental.php
+++ wp-content/themes/uwsod-2018/searchdental.php
@@ -170,22 +170,25 @@ padding-left: 0px !important;
 
         }
     </style>
-   <!-- Google Tag Manager -->
+<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KC3TVQJ');</script>
 <!-- End Google Tag Manager -->
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G23S47FHYX"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-25627765-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
+  gtag('config', 'UA-25627765-1');
   gtag('config', 'G-G23S47FHYX');
 </script>
+    
     <script>
         (function () {
             var cx = '013161175624235233741:pwunvv-3nuo';
